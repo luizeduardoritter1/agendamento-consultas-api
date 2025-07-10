abb20268f887e63eb4198978240d707b7b2e0620
luizeduardoritter1 <luizeduardoritter6@gmail.com>
2025-07-10 11:42:44 -0300

    Projeto inicial gerado com Spring Initializr

---
+++ src/main/java/com/app/agendamentoConsultaApi/AgendamentoConsultaApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.agendamentoConsultaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoConsultaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoConsultaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/agendamentoConsultaApi/AgendamentoConsultaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.agendamentoConsultaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoConsultaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
